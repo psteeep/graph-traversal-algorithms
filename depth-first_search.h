d3e29609de8e5813918ce51ca35225e50ce14e64
psteeep <psteeep@users.noreply.github.com>
2021-02-10 11:03:56 +0200

    add depth-first search algo

---
+++ depth-first_search.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <list>
+
+
+class Graph {
+private:
+
+    int V; 
+
+    std::list<int>* adj;
+
+    void DFSUtil(int v, bool visited[]);
+
+public:
+    Graph(int V); 
+    void addEdge(int v, int w);
+    void DFS(int v);
+};
--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 #include "breadth-first_search.h"
+#include "depth-first_search.h"
 
 int main() {
     
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
-
-    std::cout << "Following is Breadth First Traversal "
-        << "(starting from vertex 2) \n";
-    g.BFS(2);
+  
 
 	return 0;
 }
