15c111ccdfade635f20647a449653e0638234909
psteeep <psteeep@users.noreply.github.com>
2021-03-13 13:23:33 +0200

    add graphic

--- main.cpp
+++ main.cpp
@@ -1,61 +1,102 @@
+#include <windows.h>
 #include <iostream>
-#include "breadth_depth-first_search.h"
-#include "graph_algo.h"
+#include <glut.h>
+#include <cstdlib>
+#include <cmath>
 
-int main() {
-    
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
+using namespace std;
 
-    std::cout << "Following is Breadth First Traversal "
-        << "(starting from vertex 2) \n";
-    g.BFS(2);
-  
-    std::cout << std::endl;
+const int X_COORD = 50;// X - размерность ] должны
+const int Y_COORD = 50;// Y - размерность ] быть равными
+const float ITERATIONS = 0.00005;// прорисовка графика (чем меньше тем лучше)
 
-    std::cout << "Following is Depth First Traversal"
-        " (starting from vertex 2) \n";
-    g.DFS(2);
+int x_off = X_COORD / 2;// начало
+int y_off = Y_COORD / 2;// оси координат
 
-    std::cout << std::endl;
+//исходная функция
+#define expr x
 
-    GraphAlgo algo;
+void drawgrid(float SERIF_OFFSET, float SERIF_DISTANCE) {
+    glBegin(GL_LINES);
+    //задаем цвета
+    glColor3f(0.0, 0.0, 0.0);
 
-    int graph[V][V] = { { 0, 2, 0, 6, 0 },
-                       { 2, 0, 3, 8, 5 },
-                       { 0, 3, 0, 0, 7 },
-                       { 6, 8, 0, 0, 9 },
-                       { 0, 5, 7, 9, 0 } };
-  
-    algo.primMST(graph);
-    
-    
-    
-    int graph1[D][D] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                        { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                        { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                        { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                        { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                        { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                        { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                        { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+    //рисуем координатные оси
+    //горизонталь
+    glVertex2f(0.0, Y_COORD / 2);
+    glVertex2f(X_COORD, Y_COORD / 2);
+    //засечки по горизонтали
+    int p = X_COORD / 2;
+    for (int i = X_COORD / 2; i < X_COORD; i += SERIF_DISTANCE, p -= SERIF_DISTANCE) {
+        glVertex2f(i, Y_COORD / 2);
+        glVertex2f(i, Y_COORD / 2 + SERIF_OFFSET);
 
-    algo.dijkstra(graph1, 0);
+        glVertex2f(p, Y_COORD / 2);
+        glVertex2f(p, Y_COORD / 2 + SERIF_OFFSET);
+    }
+    //вертикаль
+    int t = Y_COORD / 2;
+    glVertex2f(X_COORD / 2, Y_COORD);
+    glVertex2f(X_COORD / 2, 0.0);
+    //засечки по вертикали
+    for (int i = Y_COORD / 2; i < Y_COORD; i += SERIF_DISTANCE, t -= SERIF_DISTANCE) {
+        glVertex2f(X_COORD / 2, i);
+        glVertex2f(Y_COORD / 2 + SERIF_OFFSET, i);
 
-    int graph2[F][F] = { {0, 16, 13, 0, 0, 0},
-                       {0, 0, 10, 12, 0, 0},
-                       {0, 4, 0, 0, 14, 0},
-                       {0, 0, 9, 0, 0, 20},
-                       {0, 0, 0, 7, 0, 4},
-                       {0, 0, 0, 0, 0, 0}
-    };
+        glVertex2f(X_COORD / 2, t);
+        glVertex2f(Y_COORD / 2 + SERIF_OFFSET, t);
+    }
+    glEnd();
+}
 
-    std::cout << "The maximum possible flow is " << algo.fordFulkerson(graph2, 0, 5);
-    return 0;
+void drawfunc() {
+    //рисуем график
+    glBegin(GL_POINTS);
+    float j = 0;
+    glColor3f(0.8, 0.0, 0.8);
+    for (float x = -X_COORD * 2; x < X_COORD * 2; x += ITERATIONS) {
+        //перерасчитываем координаты
+        j = expr;
+        glVertex2f(x_off + x, y_off + j);//не убирать x и y!! это оффсет по осям!
+    }
+    glEnd();
+}
+
+void funcinfo(int val1, int val2) {
+    //информация о графике
+    for (float x = val1; x <= val2; x++) {
+        float j = expr;
+        cout << x << " : " << j << endl;
+    }
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    cout << "Osnovnie toshki po vashemu grafiku: \n";
+
+    drawgrid(0.3, 5);
+    drawfunc();
+    funcinfo(-5, 5);
+
+    glutSwapBuffers();
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(500, 200);
+    glutCreateWindow("GLUT_TESTING_APP");
+
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //пространство координат
+    glOrtho(0.0, X_COORD, 0.0, Y_COORD, -1.0, 1.0);
+
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
